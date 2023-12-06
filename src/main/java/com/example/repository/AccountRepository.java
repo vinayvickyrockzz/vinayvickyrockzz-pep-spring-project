66e5fcb2db1b38af9c75f783d547895b0a80bbb6
vinayvickyrockzz <vinayvickyrockzz@gmail.com>
2023-12-06 12:24:14 +0530

    testcasepassed

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,4 +1,20 @@
 package com.example.repository;
 
-public interface AccountRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.entity.Account;
+
+public interface AccountRepository extends JpaRepository<Account, Integer>{
+
+    //@Modifying
+    //@Transactional
+    /*@Query("INSERT INTO Account(username,password) vaules (?,?)")
+    void newUserAccountRegistration(@Param("username") String username, @Param("password") String password);
+    @Query("SELECT username FROM Account WHERE username=?")
+    public String getUserNameString(Account account); */
+    @Query("FROM Account WHERE username=:username")
+    public Account getAccountByUsername(@Param("username") String username);
+    
 }
